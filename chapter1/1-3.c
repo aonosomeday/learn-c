7402a105710a7bfcb0c7dd45a43755d8debe8aaf
aonosomeday <180724451+aonosomeday@users.noreply.github.com>
2024-10-10 22:58:22 +0900

    complete chapter 1

---
+++ chapter1/1-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Problem:\n");
+    printf("%d apples\n", 4);
+    printf("%d apples\n", 9);
+    printf("%d apples\n", 16);
+
+    printf("Answer:\n");
+    printf("%d apples\n%d apples\n%d apples\n", 4, 9, 16);
+    return 0;
+}
